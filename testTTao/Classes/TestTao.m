e71219db6d06bbb6efcaf44fd2ccd58846d9f985
tiantao <tiantao@userdeM12.local>
2017-02-28 15:47:08 +0800

    1

---
+++ testTTao/Classes/TestTao.m
@@ -0,0 +1,20 @@
+//
+//  TestTao.m
+//  testTTao
+//
+//  Created by tiantao on 17/2/28.
+//  Copyright © 2017年 tiantao. All rights reserved.
+//
+
+#import "TestTao.h"
+
+@implementation TestTao
++ (void)log;
+{
+
+
+    NSLog(@"1");
+    NSLog(@"2");
+
+}
+@end
